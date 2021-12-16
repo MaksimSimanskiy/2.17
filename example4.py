5ff561c6ab3d4fd7c79107d5bd2630411a6e4845
MaksimSimanskiy <78483044+MaksimSimanskiy@users.noreply.github.com>
2021-12-16 14:16:28 +0300

    Add files via upload

---
+++ example4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="increase output verbosity",
+        action="store_true"
+    )
+    args = parser.parse_args()
+    if args.verbose:
+        print("verbosity turned on")
